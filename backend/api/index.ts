8dd3756065cdebee27e1cf077edb16ddb5e77085
psirvarolli <psirvarolli@gmail.com>
2025-09-11 17:27:12 -0300

    teste

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,17 +1,25 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { buildApp } from '../src/app.js'; 
+import { buildApp } from '../src/app.js';
 
 let fastifyApp: any;
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     if (!fastifyApp) {
+      console.log("⚡ Inicializando Fastify app...");
       fastifyApp = await buildApp();
       await fastifyApp.ready();
+      console.log("✅ Fastify pronto!");
     }
+
     fastifyApp.server.emit('request', req, res);
-  } catch (err) {
-    console.error('Fastify error:', err);
-    res.status(500).json({ error: 'Internal Server Error 1' });
+  } catch (err: any) {
+    console.error("🔥 Fastify error:", err);
+
+    res.status(500).json({
+      error: "Internal Server Error 2",
+      message: err?.message,
+      stack: err?.stack
+    });
   }
 }
